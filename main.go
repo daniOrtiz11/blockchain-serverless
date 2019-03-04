9e7fddb220d8ac053e765e0d568dd591b176e60e
Unknown <dani_glvz@hotmail.es>
2019-03-04 19:35:45 +0100

    unified single main.go

--- main.go
+++ main.go
@@ -2,132 +2,319 @@ package main
 
 import (
 	"bufio"
+	"context"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/hex"
 	"encoding/json"
+	"flag"
+	"fmt"
 	"io"
 	"log"
-	"net"
+	mrand "math/rand"
+	"os"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 
+	crypto "gx/ipfs/QmTW4SdgBWq9GjsBsHeUx8WuGxzhgzAf88UMH2w62PC8yK/go-libp2p-crypto"
+	ma "gx/ipfs/QmTZBfrPJmjWsCvHEtX5FE6KimVJhsJg5sBbqEFYf4UZtL/go-multiaddr"
+	net "gx/ipfs/QmY3ArotKMKaL7YGfbQfyDrib6RVraLqZYWXZvVgZktBxp/go-libp2p-net"
+	peer "gx/ipfs/QmYVXrKrKHDC9FobgmcmshCDyWwdrfwfanNQN4oxJ9Fk3h/go-libp2p-peer"
+	host "gx/ipfs/QmYrWiWM4qtrnCeT3R14jY3ZZyirDNJgwK57q4qFYePgbd/go-libp2p-host"
+	pstore "gx/ipfs/QmaCTz9RkrU13bm9kMB54f7atgqM4qkjDZpRwRoJiWXEqs/go-libp2p-peerstore"
+	golog "gx/ipfs/QmbkT7eMTyXfpeyB3ZMxxcxg7XH8t6uXp49jqzz4HB7BGF/go-log"
+	gologging "gx/ipfs/QmcaSwFc5RBg8yCq54QURwEU4nwjfCpjbpmaAm4VbdGLKv/go-logging"
+
 	"github.com/davecgh/go-spew/spew"
+	libp2p "github.com/libp2p/go-libp2p"
 )
 
-/*
-godotoenv-> get params from configuration files
-spew -> print go structures
-bufio -> buffer to read data
-io -> handle out console
-net -> network interface (TCP currently)
-os -> read from file
-strconv -> string conversion
-sync -> dependencie to use mutex
-*/
-
-// Custom Block
+// Block represents each 'item' in the blockchain
 type Block struct {
-	Index         int
-	Timestamp     string
-	Private_value int
-	Hash          string
-	PrevHash      string
+	Index     int
+	Timestamp string
+	BPM       int
+	Hash      string
+	PrevHash  string
 }
 
-// Array Blocks -> real Blockchain
+// Blockchain is a series of validated Blocks
 var Blockchain []Block
 
-//channel for handle current access to Blockchain
-var bcServer chan []Block
-
-//mutex needed in sumalted broadcasting
 var mutex = &sync.Mutex{}
 
-func main() {
-	//check access configuration file
-	/*err := godotenv.Load("configuration.env")
-	if err != nil {
-		log.Fatal(err)
-	}*/
+// makeBasicHost creates a LibP2P host with a random peer ID listening on the
+// given multiaddress.
+func makeBasicHost(listenPort int, randseed int64) (host.Host, error) {
 
-	bcServer = make(chan []Block)
-
-	// create genesis block
-	t := time.Now()
-	genesisBlock := Block{0, t.String(), 0, "", ""}
-	spew.Dump(genesisBlock)
-	Blockchain = append(Blockchain, genesisBlock)
-
-	//get port from constant file
-	httpPort := PORT
-
-	// start TCP server
-	server, err := net.Listen("tcp", ":"+httpPort)
-	if err != nil {
-		log.Fatal(err)
+	// If the seed is zero, use real cryptographic randomness. Otherwise, use a
+	// deterministic randomness source to make generated keys stay the same
+	// across multiple runs
+	var r io.Reader
+	/*
+		Seed to create a random address for our host.
+	*/
+	if randseed == 0 {
+		r = rand.Reader
+	} else {
+		r = mrand.New(mrand.NewSource(randseed))
 	}
-	log.Println("HTTP Server Listening on port :", httpPort)
-	defer server.Close()
 
-	//Infinite loop to accetp and handle cons
+	// Generate a key pair for this host. We will use it
+	// to obtain a valid host ID.
+	priv, _, err := crypto.GenerateKeyPairWithReader(crypto.RSA, 2048, r)
+	if err != nil {
+		return nil, err
+	}
+
+	/*
+		opts: address, identity to new connect between peer.
+	*/
+
+	addrstr := fmt.Sprintf("/ip4/127.0.0.1/tcp/%d", listenPort)
+
+	opts := []libp2p.Option{
+		libp2p.ListenAddrStrings(addrstr),
+		libp2p.Identity(priv),
+	}
+
+	basicHost, err := libp2p.New(context.Background(), opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	// Build host multiaddress
+	hostAddr, _ := ma.NewMultiaddr(fmt.Sprintf("/ipfs/%s", basicHost.ID().Pretty()))
+
+	// Now we can build a full multiaddress to reach this host
+	// by encapsulating both addresses:
+	addrs := basicHost.Addrs()
+	var addr ma.Multiaddr
+	findaddr := false
+	// select the address starting with "ip4"
+	for _, i := range addrs {
+		if strings.HasPrefix(i.String(), "/ip4") {
+			addr = i
+			findaddr = true
+			break
+		}
+	}
+	findaddr = false
+	if findaddr == false {
+		log.Printf("In if")
+		addrcero := basicHost.Addrs()[0]
+		fullAddrcero := addrcero.Encapsulate(hostAddr)
+		sfulladdr := fmt.Sprintf("%s", fullAddrcero)
+		s := strings.Split(sfulladdr, "/")
+		nextkey := s[3]
+		if nextkey == "tcp" {
+			nextkey = s[6]
+		}
+		addrstr := fmt.Sprintf("/ip4/127.0.0.1/tcp/%d", listenPort)
+		realaddress := addrstr + "/ipfs/" + nextkey
+		log.Printf("I am %s\n", fullAddrcero)
+		log.Printf("Now run \"go run main.go -l %d -d %s\" on a different terminal\n", listenPort+1, realaddress)
+	} else {
+		fullAddr := addr.Encapsulate(hostAddr)
+		log.Printf("I am %s\n", fullAddr)
+		log.Printf("Now run \"go run main.go -l %d -d %s\" on a different terminal\n", listenPort+1, fullAddr)
+
+	}
+
+	return basicHost, nil
+}
+
+func handleStream(s net.Stream) {
+
+	log.Println("Got a new stream!")
+
+	// Create a buffer stream for non blocking read and write.
+	rw := bufio.NewReadWriter(bufio.NewReader(s), bufio.NewWriter(s))
+
+	go readData(rw)
+	go writeData(rw)
+
+	// stream 's' will stay open until you close it (or the other side closes it).
+}
+
+func readData(rw *bufio.ReadWriter) {
+
 	for {
-		conn, err := server.Accept()
+		str, err := rw.ReadString('\n')
 		if err != nil {
 			log.Fatal(err)
 		}
-		go handleConn(conn)
+
+		if str == "" {
+			return
+		}
+		if str != "\n" {
+
+			chain := make([]Block, 0)
+			if err := json.Unmarshal([]byte(str), &chain); err != nil {
+				log.Fatal(err)
+			}
+
+			mutex.Lock()
+			if len(chain) > len(Blockchain) {
+				Blockchain = chain
+				bytes, err := json.MarshalIndent(Blockchain, "", "  ")
+				if err != nil {
+
+					log.Fatal(err)
+				}
+				// Green console color: 	\x1b[32m
+				// Reset console color: 	\x1b[0m
+				fmt.Printf("\x1b[32m%s\x1b[0m> ", string(bytes))
+			}
+			mutex.Unlock()
+		}
+	}
+}
+
+func writeData(rw *bufio.ReadWriter) {
+
+	go func() {
+		for {
+			time.Sleep(5 * time.Second)
+			mutex.Lock()
+			bytes, err := json.Marshal(Blockchain)
+			if err != nil {
+				log.Println(err)
+			}
+			mutex.Unlock()
+
+			mutex.Lock()
+			rw.WriteString(fmt.Sprintf("%s\n", string(bytes)))
+			rw.Flush()
+			mutex.Unlock()
+
+		}
+	}()
+
+	stdReader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("> ")
+		sendData, err := stdReader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		sendData = strings.Replace(sendData, "\n", "", -1)
+		bpm, err := strconv.Atoi(sendData)
+		if err != nil {
+			log.Fatal(err)
+		}
+		newBlock := generateBlock(Blockchain[len(Blockchain)-1], bpm)
+
+		if isBlockValid(newBlock, Blockchain[len(Blockchain)-1]) {
+			mutex.Lock()
+			Blockchain = append(Blockchain, newBlock)
+			mutex.Unlock()
+		}
+
+		bytes, err := json.Marshal(Blockchain)
+		if err != nil {
+			log.Println(err)
+		}
+
+		spew.Dump(Blockchain)
+
+		mutex.Lock()
+		rw.WriteString(fmt.Sprintf("%s\n", string(bytes)))
+		rw.Flush()
+		mutex.Unlock()
 	}
 
 }
 
-func handleConn(conn net.Conn) {
+func main() {
+	t := time.Now()
+	genesisBlock := Block{}
+	genesisBlock = Block{0, t.String(), 0, calculateHash(genesisBlock), ""}
 
-	defer conn.Close()
+	Blockchain = append(Blockchain, genesisBlock)
 
-	io.WriteString(conn, "Enter a new Private_value:")
+	// LibP2P code uses golog to log messages. They log with different
+	// string IDs (i.e. "swarm"). We can control the verbosity level for
+	// all loggers with:
+	golog.SetAllLoggers(gologging.INFO) // Change to DEBUG for extra info
 
-	scanner := bufio.NewScanner(conn)
+	// Parse options from the command line
+	listenF := flag.Int("l", 0, "wait for incoming connections")
+	target := flag.String("d", "", "target peer to dial")
+	seed := flag.Int64("seed", 0, "set random seed for id generation")
+	flag.Parse()
 
-	// take in Private_value from stdin and add it to blockchain after conducting necessary validation
-	go func() {
-		for scanner.Scan() {
-			Private_value, err := strconv.Atoi(scanner.Text())
-			if err != nil {
-				log.Printf("%v not a number: %v", scanner.Text(), err)
-				continue
-			}
-			newBlock, err := generateBlock(Blockchain[len(Blockchain)-1], Private_value)
-			if err != nil {
-				log.Println(err)
-				continue
-			}
-			if isBlockValid(newBlock, Blockchain[len(Blockchain)-1]) {
-				newBlockchain := append(Blockchain, newBlock)
-				replaceChain(newBlockchain)
-			}
-
-			bcServer <- Blockchain
-			io.WriteString(conn, "\nEnter a new Private_value:")
-		}
-	}()
-
-	// simulate receiving broadcast
-	go func() {
-		for {
-			time.Sleep(30 * time.Second)
-			mutex.Lock()
-			output, err := json.Marshal(Blockchain)
-			if err != nil {
-				log.Fatal(err)
-			}
-			mutex.Unlock()
-			io.WriteString(conn, string(output))
-		}
-	}()
-
-	for _ = range bcServer {
-		spew.Dump(Blockchain)
+	if *listenF == 0 {
+		log.Fatal("Please provide a port to bind on with -l")
 	}
 
+	// Make a host that listens on the given multiaddress
+	ha, err := makeBasicHost(*listenF, *seed)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if *target == "" {
+		log.Println("listening for connections")
+		// Set a stream handler on host A. /p2p/1.0.0 is
+		// a user-defined protocol name.
+		ha.SetStreamHandler("/p2p/1.0.0", handleStream)
+
+		select {} // hang forever
+		/**** This is where the listener code ends ****/
+	} else {
+		ha.SetStreamHandler("/p2p/1.0.0", handleStream)
+
+		// The following code extracts target's peer ID from the
+		// given multiaddress
+		ipfsaddr, err := ma.NewMultiaddr(*target)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		pid, err := ipfsaddr.ValueForProtocol(ma.P_IPFS)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		peerid, err := peer.IDB58Decode(pid)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		// Decapsulate the /ipfs/<peerID> part from the target
+		// /ip4/<a.b.c.d>/ipfs/<peer> becomes /ip4/<a.b.c.d>
+		targetPeerAddr, _ := ma.NewMultiaddr(
+			fmt.Sprintf("/ipfs/%s", peer.IDB58Encode(peerid)))
+		targetAddr := ipfsaddr.Decapsulate(targetPeerAddr)
+
+		// We have a peer ID and a targetAddr so we add it to the peerstore
+		// so LibP2P knows how to contact it
+		ha.Peerstore().AddAddr(peerid, targetAddr, pstore.PermanentAddrTTL)
+
+		log.Println("opening stream")
+		// make a new stream from host B to host A
+		// it should be handled on host A by the handler we set above because
+		// we use the same /p2p/1.0.0 protocol
+		s, err := ha.NewStream(context.Background(), peerid, "/p2p/1.0.0")
+		if err != nil {
+			log.Fatalln(err)
+		}
+		// Create a buffered stream so that read and writes are non blocking.
+		rw := bufio.NewReadWriter(bufio.NewReader(s), bufio.NewWriter(s))
+
+		// Create a thread to read and write data.
+		go writeData(rw)
+		go readData(rw)
+
+		select {} // hang forever
+
+	}
 }
 
 // make sure block is valid by checking index, and comparing the hash of the previous block
@@ -140,24 +327,24 @@ func isBlockValid(newBlock, oldBlock Block) bool {
 		return false
 	}
 
-	if CalculateHash(newBlock) != newBlock.Hash {
+	if calculateHash(newBlock) != newBlock.Hash {
 		return false
 	}
 
 	return true
 }
 
-// make sure the chain we're checking is longer than the current blockchain
-func replaceChain(newBlocks []Block) {
-	mutex.Lock()
-	if len(newBlocks) > len(Blockchain) {
-		Blockchain = newBlocks
-	}
-	mutex.Unlock()
+// SHA256 hashing
+func calculateHash(block Block) string {
+	record := strconv.Itoa(block.Index) + block.Timestamp + strconv.Itoa(block.BPM) + block.PrevHash
+	h := sha256.New()
+	h.Write([]byte(record))
+	hashed := h.Sum(nil)
+	return hex.EncodeToString(hashed)
 }
 
 // create a new block using previous block's hash
-func generateBlock(oldBlock Block, Private_value int) (Block, error) {
+func generateBlock(oldBlock Block, BPM int) Block {
 
 	var newBlock Block
 
@@ -165,9 +352,9 @@ func generateBlock(oldBlock Block, Private_value int) (Block, error) {
 
 	newBlock.Index = oldBlock.Index + 1
 	newBlock.Timestamp = t.String()
-	newBlock.Private_value = Private_value
+	newBlock.BPM = BPM
 	newBlock.PrevHash = oldBlock.Hash
-	newBlock.Hash = CalculateHash(newBlock)
+	newBlock.Hash = calculateHash(newBlock)
 
-	return newBlock, nil
+	return newBlock
 }
